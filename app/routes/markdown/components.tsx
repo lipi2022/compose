6a1b6d9950d16533a1025d629f2110ce729ec0cf
lipicoder <lipeng@zepan.com.cn>
2022-02-14 23:22:57 +0800

    clean

---
+++ app/routes/markdown/components.tsx
@@ -0,0 +1,63 @@
+import React, { Ref, PropsWithChildren } from "react";
+import ReactDOM from "react-dom";
+import { cx, css } from "@emotion/css";
+
+interface BaseProps {
+  className: string;
+  [key: string]: unknown;
+}
+type OrNull<T> = T | null;
+
+export const ToolButton = React.forwardRef(
+  (
+    {
+      className,
+      active,
+      reversed,
+      ...props
+    }: PropsWithChildren<
+      {
+        active: boolean;
+        reversed: boolean;
+      } & BaseProps
+    >,
+    ref: Ref<HTMLSpanElement>
+  ) => (
+    <span
+      {...props}
+      ref={ref}
+      className={cx(
+        className,
+        css`
+          cursor: pointer;
+          color: ${reversed
+            ? active
+              ? "white"
+              : "#aaa"
+            : active
+            ? "black"
+            : "#ccc"};
+        `
+      )}
+    />
+  )
+);
+
+export const SvgIcon = React.forwardRef(
+  (
+    { className, ...props }: PropsWithChildren<BaseProps>,
+    ref: Ref<HTMLSpanElement>
+  ) => (
+    <span
+      {...props}
+      ref={ref}
+      className={cx(
+        "material-icons",
+        className,
+        css`
+          hover: bg-slate-300 h-8 w-8 flex items-center justify-center;
+        `
+      )}
+    />
+  )
+);
--- app/routes/markdown/index.tsx
+++ app/routes/markdown/index.tsx
@@ -9,14 +9,81 @@ import {
   RiCodeSSlashFill,
   RiChatQuoteLine,
 } from "react-icons/ri";
+import {
+  Editor,
+  Transforms,
+  createEditor,
+  Descendant,
+  Element as SlateElement,
+} from "slate";
+import { withHistory } from "slate-history";
+import { Editable, withReact, useSlate, Slate } from "slate-react";
+import { ToolButton, SvgIcon } from "./components";
+import isHotkey from "is-hotkey";
+
+const HOTKEYS = {
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
+};
+
+const LIST_TYPES = ["numbered-list", "bulleted-list"];
+
+const isBlockActive = (editor, format) => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: (n) =>
+        !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format,
+    })
+  );
+
+  return !!match;
+};
+
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    Editor.removeMark(editor, format);
+  } else {
+    Editor.addMark(editor, format, true);
+  }
+};
+
+const MarkButton = ({ format, icon }) => {
+  // const editor = useSlate();
+  const editor = ""; // to change
+  return (
+    <ToolButton
+      active={isMarkActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleMark(editor, format);
+      }}
+    >
+      <SvgIcon>{icon}</SvgIcon>
+    </ToolButton>
+  );
+};
 
 export default function Index() {
   return (
     <div className="container bg-slate-100  h-screen pt-32 mx-auto flex justify-center">
       <div className="bg-white w-6/12 h-64 shadow-md rounded-lg divide-y-2 divide-slate-50">
         <div className="h-10 flex  items-center gap-4 pl-1">
+          <MarkButton format="bold" icon="format_bold"></MarkButton>
           <div className="hover:bg-slate-300 h-8 w-8 flex items-center justify-center">
-            <RiBold size={22} color="gray" title="bold" />
+            <RiBold size={20} color="gray" title="bold" />
           </div>
           <RiItalic size={22} color="gray" title="bold" />
           <RiCodeSSlashFill size={22} color="gray" title="bold" />
