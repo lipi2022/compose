ea9328488a836aa21d5f8719eb0ff207b6710402
dialling <lipeng.sh@qq.com>
2022-02-14 11:58:27 +0800

    new

--- app/routes/markdown/index.tsx
+++ app/routes/markdown/index.tsx
@@ -1,11 +1,46 @@
+import { IconContext } from "react-icons";
+import {
+  RiBold,
+  RiItalic,
+  RiListUnordered,
+  RiListOrdered,
+  RiImageAddLine,
+  RiSaveLine,
+  RiCodeSSlashFill,
+  RiChatQuoteLine,
+} from "react-icons/ri";
+
 export default function Index() {
   return (
-    <div className="container mx-auto px-4">
-      <div className="columns-2">
-        <p>Well, let me tell you something, ...</p>
-        <p className="break-after-column">Sure, go ahead, laugh...</p>
-        <p>Maybe we can live without...</p>
-        <p>Look. If you think this is...</p>
+    <div className="bg-slate-200 h-screen flex justify-center pt-32">
+      <div className="bg-white w-3/5 h-64 shadow-md rounded-md">
+        <div className="grid grid-cols-1 ">
+          <div className="bg-slate-100 h-12 flex justify-center">
+            <div className="grid grid-cols-9 divide-x justify-center h-full w-full">
+              <div className="place-content-center">
+                <IconContext.Provider
+                  value={{
+                    color: "color",
+                    className: "",
+                  }}
+                >
+                  <div>
+                    <RiBold />
+                  </div>
+                </IconContext.Provider>
+              </div>
+              <div className="text-center">bold</div>
+              <div>italic</div>
+              <div>code</div>
+              <div>blockquote</div>
+              <div>ul</div>
+              <div>ol</div>
+              <div>image</div>
+              <div>save</div>
+            </div>
+          </div>
+          <div className="white h-full"></div>
+        </div>
       </div>
     </div>
   );
