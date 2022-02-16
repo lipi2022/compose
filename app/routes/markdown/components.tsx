8ef494a6fe0d437fc4b19d9de462a485a7125e94
dialling <lipeng.sh@qq.com>
2022-02-16 21:09:28 +0800

    good

--- app/routes/markdown/components.tsx
+++ app/routes/markdown/components.tsx
@@ -1,6 +1,28 @@
 import React, { Ref, PropsWithChildren } from "react";
 import ReactDOM from "react-dom";
 import { cx, css } from "@emotion/css";
+import {
+  Editor,
+  Transforms,
+  createEditor,
+  Descendant,
+  Element as SlateElement,
+} from "slate";
+import {
+  RiBold,
+  RiItalic,
+  RiListUnordered,
+  RiListOrdered,
+  RiImageAddLine,
+  RiSaveLine,
+  RiCodeSSlashFill,
+  RiChatQuoteLine,
+  RiBookReadLine,
+} from "react-icons/ri";
+import { CustomElement, CustomText } from "./custom-types";
+import { Icon } from "./icons";
+
+const LIST_TYPES: string[] = ["numbered-list", "bulleted-list"];
 
 interface BaseProps {
   className: string;
@@ -43,21 +65,129 @@ export const ToolButton = React.forwardRef(
   )
 );
 
-export const SvgIcon = React.forwardRef(
-  (
-    { className, ...props }: PropsWithChildren<BaseProps>,
-    ref: Ref<HTMLSpanElement>
-  ) => (
-    <span
-      {...props} // todo ,different format to iconh
-      ref={ref}
-      className={cx(
-        "material-icons",
-        className,
-        css`
-          hover: bg-slate-300 h-8 w-8 flex items-center justify-center;
-        `
-      )}
-    />
-  )
-);
+export const PureButton = ({
+  editor,
+  format,
+}: {
+  editor: Editor;
+  format: string;
+}) => {
+  return (
+    <ToolButton>
+      <SvgIcon format={format}></SvgIcon>
+    </ToolButton>
+  );
+};
+
+export const MarkButton = ({
+  editor,
+  format,
+}: {
+  editor: Editor;
+  format: string;
+}) => {
+  return (
+    <ToolButton
+      active={isMarkActive(editor, format)}
+      onMouseDown={(event: MouseEvent) => {
+        event.preventDefault();
+        toggleMark(editor, format);
+      }}
+    >
+      <SvgIcon format={format}></SvgIcon>
+    </ToolButton>
+  );
+};
+
+const isMarkActive = (editor: Editor, format: string) => {
+  const marks = Editor.marks(editor);
+  // console.log(marks);
+  return marks ? (marks as any)[format] === true : false; // to check
+};
+
+export const toggleMark = (editor: Editor, format: string) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    Editor.removeMark(editor, format);
+  } else {
+    Editor.addMark(editor, format, true);
+  }
+};
+
+export const BlockButton = ({
+  editor,
+  format,
+}: {
+  editor: Editor;
+  format: string;
+}) => {
+  return (
+    <ToolButton
+      active={isBlockActive(editor, format)}
+      onMouseDown={(event: MouseEvent) => {
+        event.preventDefault();
+        toggleBlock(editor, format);
+      }}
+    >
+      <SvgIcon format={format}></SvgIcon>
+    </ToolButton>
+  );
+};
+const isBlockActive = (editor: Editor, format: string) => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: (n) =>
+        !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format,
+    })
+  );
+
+  return !!match;
+};
+
+const toggleBlock = (editor: Editor, format: string) => {
+  const isActive = isBlockActive(editor, format);
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes(n.type),
+    split: true,
+  });
+
+  let format_type: string = isActive
+    ? "paragraph"
+    : isList
+    ? "list-item"
+    : format;
+  const newProperties: Partial<SlateElement> = {
+    type: format_type as any, // to check
+  };
+
+  Transforms.setNodes<SlateElement>(editor, newProperties);
+
+  if (!isActive && isList) {
+    // 根据不同 format ，拆开成不同的 element, todo
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block as CustomElement);
+  }
+};
+
+const SvgIcon = ({ format }: { format: string }) => {
+  let icon;
+  let color = "#bcbcbc";
+  let hover_color = "black";
+  let size = "18";
+
+  return (
+    <div className="hover:bg-slate-100 h-8 w-8 flex items-center justify-center">
+      <Icon format={format} color={color} width={size} height={size}></Icon>
+    </div>
+  );
+};
---
+++ app/routes/markdown/compose.tsx
@@ -0,0 +1,164 @@
+import React, { useCallback, useMemo, useState, MouseEvent } from "react";
+import { IconContext } from "react-icons";
+
+import {
+  Editor,
+  Transforms,
+  createEditor,
+  Descendant,
+  Element as SlateElement,
+} from "slate";
+import { withHistory } from "slate-history";
+import { Editable, withReact, useSlate, Slate } from "slate-react";
+import { ToolButton } from "./components";
+import isHotkey from "is-hotkey";
+import { CustomElement, CustomText } from "./custom-types";
+import { BlockButton, MarkButton, PureButton, toggleMark } from "./components";
+
+const HOTKEYS: { [key: string]: string } = {
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
+};
+
+const Element = ({
+  attributes,
+  children,
+  element,
+}: {
+  attributes: React.ReactNode;
+  children: React.ReactNode;
+  element: CustomElement;
+}) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    // case "heading-one":
+    //   return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    // case "numbered-list":
+    //   return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Leaf = ({
+  attributes,
+  children,
+  leaf,
+}: {
+  attributes: any;
+  children: React.ReactNode;
+  leaf: CustomText;
+}) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
+
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [
+      { text: "This is editable " },
+      { text: "rich", bold: true },
+      { text: " text, " },
+      { text: "much", italic: true },
+      { text: " better than a " },
+      { text: "<textarea>", code: true },
+      { text: "!" },
+    ],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: "bold", bold: true },
+      {
+        text: ", or add a semantically rendered block quote in the middle of the page, like this:",
+      },
+    ],
+  },
+  {
+    type: "block-quote",
+    children: [{ text: "A wise quote." }],
+  },
+  {
+    type: "paragraph",
+    children: [{ text: "Try it out for yourself!" }],
+  },
+];
+
+export default function Compose() {
+  const [value, setValue] = useState<Descendant[]>(initialValue);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
+  return (
+    <div className="bg-white w-6/12 h-64 shadow-md rounded-lg divide-y-2 divide-slate-50">
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <div className="h-10 flex  items-center gap-4 pl-2">
+          <MarkButton editor={editor} format="bold"></MarkButton>
+          <MarkButton editor={editor} format="italic"></MarkButton>
+          <MarkButton editor={editor} format="underline"></MarkButton>
+          <MarkButton editor={editor} format="code"></MarkButton>
+
+          <BlockButton editor={editor} format="paragraph"></BlockButton>
+          <BlockButton editor={editor} format="blockquote"></BlockButton>
+          <BlockButton editor={editor} format="heading-one"></BlockButton>
+          <BlockButton editor={editor} format="heading-two"></BlockButton>
+          <BlockButton editor={editor} format="heading-three"></BlockButton>
+
+          <PureButton editor={editor} format="image"></PureButton>
+          <PureButton editor={editor} format="upload"></PureButton>
+        </div>
+        <div className="pl-4 pt-2">
+          <Editable
+            renderElement={renderElement}
+            renderLeaf={renderLeaf}
+            placeholder="Enter some rich text…"
+            spellCheck
+            autoFocus
+            onKeyDown={(event) => {
+              for (const hotkey in HOTKEYS) {
+                if (isHotkey(hotkey, event as any)) {
+                  event.preventDefault();
+                  const mark = HOTKEYS[hotkey];
+                  toggleMark(editor, mark);
+                }
+              }
+            }}
+          />
+        </div>
+      </Slate>
+    </div>
+  );
+}
