85b1eb04190b903071e8eb0bbae3362128a3d765
lipicoder <lipeng@zepan.com.cn>
2022-02-18 12:43:29 +0800

    cdn

--- app/routes/markdown/imageModal.tsx
+++ app/routes/markdown/imageModal.tsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 
 export default function ImageModal({ show }: { show: boolean }) {
   return (
-    <div className="container mt-16 bg-slate-50 w-96 h-[500px]  mx-auto flex justify-center shadow-md rounded-md ">
-      <div></div>
+    <div className="container  bg-slate-300 w-screen h-screen mx-auto pt-32">
+      <div className="container  bg-slate-50 w-96 h-[388px]  mx-auto flex flex-col pt-3 justify-center shadow-md rounded-md ">
+        <p className="text-xl text-gray-600"> select image</p>
+        <img src="http://34.110.222.91/drag-and-drop.svg"></img>
+      </div>
     </div>
   );
 }
