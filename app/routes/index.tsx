2f2ad2015fd9b92a5c8db9c80ed37592547e152f
lipicoder <lipeng@zepan.com.cn>
2022-02-20 09:17:03 +0800

    image modal ready

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,9 @@
 import ImageModal from "./markdown/imageModal";
 
 export default function Index() {
-  return <ImageModal show={true} />;
+  return (
+    <div className="container  bg-slate-300 w-screen h-screen mx-auto pt-32">
+      <ImageModal show={true} />
+    </div>
+  );
 }
--- app/routes/markdown/imageModal.tsx
+++ app/routes/markdown/imageModal.tsx
@@ -4,7 +4,7 @@ export default function ImageModal({ show }: { show: boolean }) {
   const [drag, setDrag] = useState(false); // if enter drag div
   const [image, setImage] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState("");
-  const [imgClass, setImgClass] = useState("invisible");
+  const [imgVisible, setImgVisible] = useState(false);
   const [svgVisible, setSvgVisible] = useState(true);
 
   const dragStyle = drag
@@ -21,63 +21,75 @@ export default function ImageModal({ show }: { show: boolean }) {
     console.log(e.dataTransfer.files[0]);
     let f: File = e.dataTransfer.files[0];
     setImage(f);
-    setImgClass("visible");
+    setImgVisible(true);
     setSvgVisible(false);
     setPreviewUrl(URL.createObjectURL(f));
   };
 
   return (
-    <div className="container  bg-slate-300 w-screen h-screen mx-auto pt-32">
-      <div className="container pt-4 bg-slate-50 max-w-lg min-h-96 mx-auto flex flex-col  shadow-md rounded-md ">
-        <div
-          className={dragStyle}
-          onDragEnter={(e) => {
-            e.preventDefault();
-            e.persist();
-            e.stopPropagation();
-            setDrag(true);
+    <div className="container pt-4 bg-slate-50 max-w-lg min-h-96 mx-auto flex flex-col  shadow-md rounded-md ">
+      <div
+        className={dragStyle}
+        onDragEnter={(e) => {
+          e.preventDefault();
+          e.persist();
+          e.stopPropagation();
+          setDrag(true);
+        }}
+        onDragLeave={() => {
+          {
+            setDrag(false);
+            console.log("drag leave");
+          }
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+          e.persist();
+          e.stopPropagation();
+          setDrag(true);
+        }}
+        onDrop={handleDrop}
+      >
+        {svgVisible ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={svgStyle}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            pointerEvents="none"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1}
+              d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+        ) : null}
+        {imgVisible ? (
+          <img className="animate-pulse" src={previewUrl}></img>
+        ) : null}
+        <div className="text-lg text-center text-gray-600 pointer-events-none">
+          {" "}
+          Drag and drop or click here
+          <br></br>
+          <p className="text-sm text-slate-400">to upload image (max 5MiB)</p>
+        </div>{" "}
+      </div>
+      <div className="flex flex-row">
+        <button
+          className="rounded-md mx-auto mb-4 bg-slate-200 w-48 h-10 text-slate-800"
+          onClick={() => {
+            setSvgVisible(true);
+            setImgVisible(false);
+            setDrag(false);
           }}
-          onDragLeave={() => {
-            {
-              setDrag(false);
-              console.log("drag leave");
-            }
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
-            e.persist();
-            e.stopPropagation();
-            setDrag(true);
-          }}
-          onDrop={handleDrop}
         >
-          {svgVisible ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={svgStyle}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              pointerEvents="none"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-          ) : null}
-          <img src={previewUrl} className={imgClass}></img>
-          <div className="text-lg text-center text-gray-600 pointer-events-none">
-            {" "}
-            Drag and drop or click here
-            <br></br>
-            <p className="text-sm text-slate-400">to upload image (max 2MiB)</p>
-          </div>{" "}
-        </div>
+          reset
+        </button>
         <button className="rounded-md mx-auto mb-4 bg-teal-400 w-48 h-10 text-teal-50">
-          submit
+          upload
         </button>
       </div>
     </div>
