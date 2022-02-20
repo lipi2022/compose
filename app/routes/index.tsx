e6e4d31a8deb6bdf782d8a2df2b6e4bb7b4c4d14
lipicoder <lipeng@zepan.com.cn>
2022-02-20 10:35:31 +0800

    main page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,18 @@
-import ImageModal from "./markdown/imageModal";
+import { Link } from "remix";
 
 export default function Index() {
   return (
-    <div className="container  bg-slate-300 w-screen h-screen mx-auto pt-32">
-      <ImageModal show={true} />
+    <div id="main" className="container bg-teal-200 w-screen h-full ">
+      <div id="menu" className="bg-slate-200 flex w-screen p-4">
+        <Link
+          to="/"
+          className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-teal-500 to-pink-600"
+        >
+          excerpt
+        </Link>
+        <div className="flex flex-grow items-center content-center pr-2"></div>
+        <div className="flex items-center ">login</div>
+      </div>
     </div>
   );
 }
