ca00d438113c083f9ed71bd7890ff74478306188
dialling <lipeng.sh@qq.com>
2022-02-21 12:51:59 +0800

    a new banner

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
       <div id="landing-header" className="flex p-4">
         <Link
           to="/"
-          className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-teal-500 to-pink-600"
+          className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-teal-500 to-slate-500"
         >
           excerpt
         </Link>
@@ -23,12 +23,9 @@ export default function Index() {
       >
         <div
           id="hero-header"
-          className="text-4xl  text-center mb-1 text-black font-bold"
+          className="text-5xl  text-center mb-1 text-black font-bold"
         >
-          <p>
-            <a className="text-teal-500">excerpt</a> all notes to{" "}
-            <a className="text-teal-500">one</a> place
-          </p>
+          save all notes to one place
         </div>
         <div className="text-slate-400 p-2 pb-4">
           Organiz your notes and pictures in a way where people can find them
@@ -37,6 +34,30 @@ export default function Index() {
           Get started
         </button>
         <img className="pt-6" src={heroshot}></img>
+      </div>
+      <div id="features" className="flex justify-center mb-3">
+        <div className="flex flex-col items-center pr-4 pb-1 pl-4 w-[368px] box-border">
+          <div className="text-2xl font-bold pb-2"> For you</div>
+          <div className="text-slate-400 text-md">
+            Organize your personal notes.Track your ideas and projects
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center pr-4 pb-1 pl-4 w-[368px] box-border border-l border-r border-slate-200">
+          <div className="text-2xl font-bold pb-2"> For your team</div>
+          <div className="text-slate-400 text-md">
+            Create a single source of truth for all project documentation, on
+            all stages. Turn Google Drive into a hub for everything your team is
+            working on
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center pr-4 pb-1 pl-4 w-[368px] box-border">
+          <div className="text-2xl font-bold pb-2"> For your users</div>
+          <div className="text-slate-400 text-md">
+            Knowledge bases for your Products,User Guides,FAQs
+          </div>
+        </div>
       </div>
     </div>
   );
